904d282196c12705cbab13fde0dd93fd0217d438
IacuboiAnna <70690332+IacuboiAnna@users.noreply.github.com>
2020-10-05 21:49:51 +0300

    Add files via upload

---
+++ Problema_10.py
@@ -0,0 +1,13 @@
+#Afişaţi tabla înmulţirii cu numărul n. Exemplu: pentru n=5, se va afişa pe verticală
+#1x5=5 2x5=10 3x5=15 4x5=20 5x5=25 6x5=30 7x5=35 8x5=40 9x5=45 10x5=50.
+n=int(input("Dati numarul:"))
+print("1 *",n,"=",1*n)
+print("2 *",n,"=",2*n)
+print("3 *",n,"=",3*n)
+print("4 *",n,"=",4*n)
+print("5 *",n,"=",5*n)
+print("6 *",n,"=",6*n)
+print("7 *",n,"=",7*n)
+print("8 *",n,"=",8*n)
+print("9 *",n,"=",9*n)
+print("10 *",n,"=",10*n)
